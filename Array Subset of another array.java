1f88e293d57ff8167fd087143566c991e9ab53d7
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-01-25 12:15:50 +0530

    Create Array Subset of another array.java

---
+++ Array Subset of another array.java
@@ -0,0 +1,42 @@
+
+
+//User function Template for Java
+
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        
+        
+        Map<Long,Integer>map=new HashMap<>();
+        
+        
+        for(long x:a1)
+        {
+            if(map.containsKey(x))
+            {
+                map.put(x,map.get(x)+1);
+            }
+            else
+            {
+                map.put(x,1);
+            }
+        }
+        
+        for(long x:a2)
+        {
+            
+            if(map.containsKey(x))
+            {
+                if(map.get(x)==1)
+                map.remove(x);
+                else
+                map.put(x,map.get(x)-1);
+                
+            }
+            else
+            return "No";
+        }
+        
+        return "Yes";
+    }
+}
