3f53a359c27333f9bb2efe34c244996cfbb6f3b4
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-03-12 19:25:59 +0530

    Create Majority Element.java
    https://www.geeksforgeeks.org/problems/majority-element-1587115620/1?page=1&category=Arrays,Strings&sortBy=submissions

---
+++ Majority Element.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+        // your code here
+        int count=0;
+        int candidate=0;
+
+        for(int num:a)
+        {
+            if(count == 0)
+            {
+                candidate=num;
+            }
+
+           if(num == candidate)
+            count++;
+            else
+            count--;
+        }
+
+        for(int i = 0; i<size; i++){
+             if(a[i]==candidate){
+                  count++;
+             }
+        }
+         if(count>size/2){
+              return candidate;
+         }
+         else{
+              return -1;
+         }
+    }
+}
